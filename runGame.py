c8897415f5c317539232e6b9d3cab9a95ac686ad
zHigor33 <higorpinho7r@gmail.com>
2025-01-24 17:23:50 -0300

    Modificando feedbacks finais

--- runGame.py
+++ runGame.py
@@ -17,14 +17,29 @@ def runGame(option: str) -> None:
 
    game.displayProgress()
 
+   if len(game.correctList) == 71:
+      print("Extraordinário! você acertou todas as letras!")
+
+      if elapsed_time <= 240: 
+         print(f"Você concluiu em {elapsed_time:.2f} segundos. Você está muito rápido!")
+
+      elif elapsed_time <= 420:
+         print(f"Você concluiu em {elapsed_time:.2f} segundos. Continue praticando!")
+
+      else:  
+         print(f"Você concluiu em {elapsed_time:.2f} segundos. Com mais treino, você ficará mais rápido!")
+      
+      print(f"Acertos: {len(game.correctList)}")
+      print(f"Erros: {len(game.wrongList)}")
+
    if len(game.correctList) > len(game.wrongList):
       print(f"Parabéns! você acertou a maior parte das letras!")
 
       if elapsed_time <= 240: 
-         print(f"Parabéns! Você concluiu em {elapsed_time:.2f} segundos. Você está muito rápido!")
+         print(f"Você concluiu em {elapsed_time:.2f} segundos. Você está muito rápido!")
 
       elif elapsed_time <= 420:
-         print(f"Bom trabalho! Você concluiu em {elapsed_time:.2f} segundos. Continue praticando!")
+         print(f"Você concluiu em {elapsed_time:.2f} segundos. Continue praticando!")
 
       else:  
          print(f"Você concluiu em {elapsed_time:.2f} segundos. Com mais treino, você ficará mais rápido!")
