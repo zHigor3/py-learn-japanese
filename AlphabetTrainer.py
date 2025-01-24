1a98fa31c1406130aaf80ab43a892dfa92f2cf7e
zHigor33 <higorpinho7r@gmail.com>
2025-01-24 17:09:39 -0300

    Melhorando o feedback de erro ao tentar carregar um arquivo

--- AlphabetTrainer.py
+++ AlphabetTrainer.py
@@ -3,24 +3,32 @@ import random
 import os
 
 class AlphabetTrainer():
-   def __init__(self, option: str):
+   def __init__(self, option: str) -> None:
       self.option = option
       self.alphabet = []
       self.wrongList = []
       self.correctList = []
 
    # Carrega o alfabeto escolhido
-   def loadAlphabet(self):
+   def loadAlphabet(self) -> None:
       try:
          with open(f'./assets/{self.option}.json', 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            self.alphabet = data
+            self.alphabet = json.load(file)
+
+      except FileNotFoundError:
+         print(f"Erro: O arquivo './assets/{self.option}.json' não foi encontrado.")
+         exit(1)
+
+      except json.JSONDecodeError:
+         print(f"Erro: O arquivo './assets/{self.option}.json' contém um formato inválido.")
+         exit(1)
 
       except Exception as e:
-         print('Error --> ', e)
+         print(f"Erro inesperado: {e}")
+         exit(1)
 
    # Seleciona uma letra aleatória e pergunta qual o som dela
-   def answer(self):
+   def answer(self) -> None:
       question = random.choice(self.alphabet)
       response = input(f"Qual o som dessa letra? {question['latter']}: ")
 
@@ -31,17 +39,17 @@ class AlphabetTrainer():
          self.wrongAnswer(question)
 
    # Quando question for errado, remove letra do alfabeto e adiciona a lista de errados
-   def wrongAnswer(self, question):
+   def wrongAnswer(self, question: object) -> None:
       self.wrongList.append(question)
       self.alphabet.remove(question)
 
    # Quando question for correto, remove letra do alfabeto e adiciona a lista de corretos
-   def correctAnswer(self, question):
+   def correctAnswer(self, question: object):
       self.correctList.append(question)
       self.alphabet.remove(question)
    
    # Display de progresso do treinamento
-   def displayProgress(self):
+   def displayProgress(self) -> None:
       os.system('cls' if os.name == 'nt' else 'clear')
 
       print(f"Restam {len(self.alphabet)} letras para treinar.")
